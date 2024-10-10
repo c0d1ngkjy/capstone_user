19b150c9fdfbfb41514ecf9efd261703deede221
onCloud <modimodi@mjc.ac.kr>
2024-10-10 10:55:16 +0900

    modified main and added component
    yup

--- capstone_user/lib/main.dart
+++ capstone_user/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:capstone_user/clubListPage.dart';
+import 'package:capstone_user/communityPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -41,7 +43,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('여기모여'),
+        title: const Text('Main Page'),
       ),
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
@@ -60,79 +62,12 @@ class _HomePageState extends State<HomePage> {
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.group), label: '동아리'),
           BottomNavigationBarItem(icon: Icon(Icons.forum), label: '커뮤니티'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: '중고거래'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: '중고거래'),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: '프로필'),
         ],
       ),
     );
-  }
-}
-
-class ClubListPage extends StatelessWidget {
-  const ClubListPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ListView.builder(
-        itemCount: 10, // 임시로 10개의 아이템을 표시
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Card(
-              elevation: 4.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '동아리 ${index + 1}',
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    const Text('동아리 설명'),
-                    const SizedBox(height: 16),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        child: const Text('가입신청'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF696CFF),
-                          
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(14.0),
-                          ),
-                        ),
-                        onPressed: () {
-                          // 가입신청 로직 구현
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class CommunityPage extends StatelessWidget {
-  const CommunityPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('커뮤니티 페이지'));
   }
 }
 
